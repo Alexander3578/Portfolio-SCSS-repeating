ae04129b29982c7e9afea2933f94fa31b3dc8f59
Alexander Stasyuk <predator12007@mail.ru>
2026-05-22 12:26:32 +0300

    add some validation with react-hook-form

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -1,44 +1,137 @@
 import {SectionTitle} from '../../../components/sectionTitle/SectionTitle.tsx';
 import {CustomLink} from '../../../components/link/CustomLink.tsx';
 import './Contacts.scss'
+import {useForm} from 'react-hook-form';
+
+type ContactsField = {
+    name: string
+    email: string
+    message: string
+}
 
 export const Contacts = () => {
+
+    const {
+        handleSubmit,
+        register,
+        reset,
+        formState: {errors}
+    } = useForm<ContactsField>()
+
+    console.log(errors)
+    const onSubmit = (data: ContactsField) => {
+        console.log(data)
+        reset()
+    }
+
     return (
         <section id={'contacts'}
                  className={'contactsSection'}>
             <div className={'contactsContainer'}>
                 <SectionTitle title={'Contacts'}/>
-                <form className={'contactsForm'}>
+                <form className={'contactsForm'}
+                      onSubmit={handleSubmit(onSubmit)}>
                     <div className={'inputsWrapper'}>
+
                         <div className="formItem">
-                            <label htmlFor="inputText"
-                                   className="formLabel">Name</label>
-                            <input type="text"
-                                   name={'Name'}
-                                   className={'contactsField fieldName'}
-                                   id="inputText"
-                                   placeholder={'Example'}/>
+                            <label
+                                htmlFor="inputName"
+                                className="formLabel"
+                            >
+                                Name
+                            </label>
+
+                            <input
+                                type="text"
+                                id="inputName"
+                                placeholder="Name"
+                                className={`contactsField ${
+                                    errors.email ? 'error' : ''
+                                }`}
+                                {...register('name', {
+                                    required:
+                                        'Name is required',
+                                    minLength: {
+                                        value: 3,
+                                        message:
+                                            'Minimum 3 symbols'
+                                    }
+                                })}
+                            />
+                            {errors.name && (
+                                <span className="fieldError">
+                                    {errors.name.message}
+                                </span>
+                            )}
+                        </div>
+
+                        <div className="formItem">
+                            <label
+                                htmlFor="inputEmail"
+                                className="formLabel"
+                            >
+                                Email
+                            </label>
+
+                            <input
+                                type="email"
+                                id="inputEmail"
+                                placeholder="example@mail.ru"
+                                className={`contactsField ${
+                                    errors.email ? 'error' : ''
+                                }`}
+                                {...register('email', {
+                                    required:
+                                        'Email is required',
+                                    pattern: {
+                                        value:
+                                            /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                        message:
+                                            'Invalid email'
+                                    }
+                                })}
+                            />
+
+                            {errors.email && (
+                                <span className="fieldError">
+                                    {errors.email.message}
+                                </span>
+                            )}
                         </div>
                         <div className="formItem">
-                            <label htmlFor="inputEmail"
-                                   className="formLabel">Email</label>
-                            <input type="email"
-                                   name={'Email'}
-                                   className={'contactsField fieldEmail'}
-                                   id="inputEmail"
-                                   placeholder={'example@mail.ru'}/>
-                        </div>
-                        <div className="formItem">
-                            <label htmlFor="formTextarea"
-                                   className="formLabel">Mensaje</label>
-                            <textarea name={'Message'}
-                                      className={'contactsField'}
-                                      id="formTextarea"
-                                      placeholder={'Your message'}/>
+                            <label
+                                htmlFor="formTextarea"
+                                className="formLabel"
+                            >
+                                Message
+                            </label>
+
+                            <textarea
+                                id="formTextarea"
+                                placeholder="Your message"
+                                className={`contactsField ${
+                                    errors.message ? 'error' : ''
+                                }`}
+                                {...register('message', {
+                                    required:
+                                        'Message is required',
+                                    maxLength: {
+                                        value: 100,
+                                        message:
+                                            'Count of symbols was limited'
+                                    }
+                                })}
+                            />
+
+                            {errors.message && (
+                                <span className="fieldError">
+                                    {errors.message.message}
+                                </span>
+                            )}
                         </div>
                     </div>
-                    <CustomLink type="submit"
-                                className={'contactsBtn'}
+
+                    <CustomLink className={'contactsBtn'}
                                 title={'Submit'}
                                 as={'button'}
                                 customSize={'lg'}
